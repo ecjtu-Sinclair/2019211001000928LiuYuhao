b20fcd491c0a9f263e5fcfde254f012a88a29cb1
lyh <3356485569@qq.com>
2021-03-29 18:17:56 +0800

    HW-CE4 Commit Task1

---
+++ src/main/java/com/LiuYuhao/week4/demo/ConfigDemoServlet.java
@@ -0,0 +1,33 @@
+package com.LiuYuhao.week4.demo;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConfigDemoServlet extends HttpServlet {
+
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = getServletConfig().getInitParameter("name");
+        String studentid = getServletConfig().getInitParameter("studentid");
+        PrintWriter writer = response.getWriter();
+        writer.println("Name:"+name);
+        writer.println("StudentId:"+studentid);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+
+}
